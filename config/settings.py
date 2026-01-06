8661cd5b42d26668d774d433b0a821c8900c73b0
dislert <konstantintropin93@gmail.com>
2026-01-06 13:05:50 +0500

    модели, migration + вынос секр ключа

--- config/settings.py
+++ config/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3kimbw-b8oip8bxz446d5$envz3lh1h)e%$j%v^i%sc5n#58g0'
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'weather',
 ]
 
 MIDDLEWARE = [
--- weather/models.py
+++ weather/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
-# Create your models here.
+
+class City(models.Model):
+    name = models.CharField(max_length=100)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+
+    def __str__(self):
+        return self.name
+
+
+class WeatherSnapshot(models.Model):
+    city = models.ForeignKey(
+        City,
+        on_delete=models.CASCADE,
+        related_name='weather_snapshots'
+    )
+    temperature = models.FloatField()
+    wind_speed = models.FloatField()
+    humidity = models.IntegerField()
+    precipitation = models.FloatField()
+    date_checked = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.city.name} — {self.date_checked.strftime("%Y-%m-%d %H:%M")}'
+
+
+class RunRecommendation(models.Model):
+    weather = models.OneToOneField(
+        WeatherSnapshot,
+        on_delete=models.CASCADE,
+        related_name='run_recommendation'
+    )
+    comfort_score = models.IntegerField()
+    is_good_for_run = models.BooleanField()
+    explanation = models.TextField()
+
+    def __str__(self):
+        return f'Recommendation for {self.weather.city.name}'
